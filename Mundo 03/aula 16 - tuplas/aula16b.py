ed0e303f5af3a5d23bb4133196179f4fcaea9f76
Luís Eduardo <77198792+luiseduaardo@users.noreply.github.com>
2025-03-03 14:05:56 -0300

    Delete Mundo 03/aula 16 - tuplas directory

--- Mundo 03/aula 16 - tuplas/aula16b.py
+++
@@ -1,9 +0,0 @@
-a = (2, 5, 4)
-b = (5, 8, 1, 2)
-c = a + b               #   Junta as tuplas (obs: a + b é diferente de b + a)
-
-print(a)
-print(b)
-print(c)
-print(c.count(5))       #   Conta quantas vezes um elemento aparece dentro da tupla
-print(c.index(8))       #   Posição do elemento dentro da tupla
